81b3124e748218c436baba3702c3791d06e672e4
Mano Marks <mano.marks@gmail.com>
2018-11-12 17:58:01 -0800

    adding in a redis service and caching the results there

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM python:3.6-alpine
 WORKDIR /home/api
+COPY ./requirements.txt /home/api/
+RUN pip install -r requirements.txt
 
 COPY ./app.py /home/api/
-COPY ./requirements.txt /home/api/
+COPY ./static/* /home/api/static/
 
-RUN pip install -r requirements.txt
 
 ENTRYPOINT [ "python" ]
 
--- app.py
+++ app.py
@@ -1,43 +1,23 @@
-import flask
-from flask import request, jsonify
+from flask import Flask, request, send_from_directory
+from flask_redis import FlaskRedis
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
-
-# Create some test data for our catalog in the form of a list of dictionaries.
-products = [
-    {
-      "brand": "Anypoint",
-      "identifier": "eb8c8ca7-3c42-4489-a820-3aa138430b75",
-      "identifiers": {
-        "SKU": "UGG-BB-PUR-06"
-      },
-      "model": "Smart Slim Micro Stripe Shirt",
-      "rating": 5,
-      "description": "Shirt by ASOS Tall. Stripe woven fabric. Added stretch for comfort. Spread collar. Button placket. Slim fit - cut close to the body. Machine wash. 98% Cotton, 2% Elastane. Our model wears a size Medium Long and is 193cm/6'4\" tall",
-      "pictures": [
-        "https://launderkart.com/wp-content/uploads/2016/07/Shirt.jpg",
-        "https://cdni.llbean.net/is/image/wim/251423_47_41?wid=428&hei=494"
-      ],
-      "price": {
-        "amount": {
-          "currency": "USD",
-          "currencyValue": 34.90,
-          "name": "Amount"
-        },
-         "salesUnit": {
-          "code": "EA",
-          "name": "Each"
-        }
-      }
-    }
-  ]
-
-
+app.config["REDIS_URL"] = "redis://redis-store:6379"
+redis_store = FlaskRedis(app)
 
 # Returns all our products
 @app.route('/api/products', methods=['GET'])
 def api_all():
-    return jsonify(products)
+    try:
+      if redis_store.exists('products'):
+        return redis_store.get('products')
+      else:
+        products = app.open_resource('static/api-sample.json','r').read()
+        redis_store.set('products',products)
+        return send_from_directory(app.static_folder,'api-sample.json')
 
+    except Exception as e:
+	    return(str(e))
+    
 app.run(host="0.0.0.0", debug=True, port=5000)
