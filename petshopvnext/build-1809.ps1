bd6820cd8eefa73a1727ec55a825c1089160c3be
Elton Stoneman <elton@sixeyed.com>
2018-11-28 10:31:32 +0000

    Update to latest lsc2019 images

---
+++ petshopvnext/build-1809.ps1
@@ -0,0 +1,5 @@
+docker-compose `
+ -f ./app/docker-compose.yml `
+ -f ./app/docker-compose-1809.yml `
+ -f ./app/docker-compose-build-1809.yml `
+ build
---
+++ petshopvnext/build.ps1
@@ -0,0 +1,4 @@
+docker-compose `
+ -f ./app/docker-compose.yml `
+ -f ./app/docker-compose-build.yml `
+ build
